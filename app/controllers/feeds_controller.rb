0d3ebb807379144a6481e7bbe769b441e0fa0078
sebastian cifuentes <secifuentes@deloitte.pt>
2021-02-11 11:23:01 +0000

    add controller

---
+++ app/controllers/feeds_controller.rb
@@ -0,0 +1,13 @@
+class FeedsController < ActionController::Base
+  def import
+    # https://theaussieenglishpodcast.podigee.io/feed/mp3
+    link = params[:link]
+
+    # Importers::RssFeed.new.call(link)
+    render status: :ok, json: { message: 'import successfully' }
+  rescue StandardError => _e
+    # ideally log error with e.message
+    # render status: :internal_server_error, json: { error: e.messsage }
+    render status: :internal_server_error, json: { error: 'feed could not be imported' }
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :feeds do
+    collection do
+      post 'import', to: 'feeds#import'
+    end
+  end
 end
